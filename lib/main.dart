beae94036c39ea21192b1585d0af3d3f9d47e3fe
Umair11 <iam.umairimran@gmail.com>
2021-08-19 17:04:42 +0500

    intial commit

---
+++ lib/main.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:porfolio/utils.dart';
+//import 'package:portfolio_app/utils.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio App Example',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+Widget iconButton(icon){
+  return IconButton(
+    icon: FaIcon(icon,color: Colors.grey,size: 20,),
+    onPressed: (){},
+  );
+}
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+   Utils _utils = Utils();
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10.0, 20, 10, 0),
+            child:Image.asset('images/black.jpg'),
+
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [.5, .6],
+                    colors: [Colors.transparent, Colors.black])),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 300,
+              child: Column(
+                children: [
+                  Text('Hello I am',
+                      style:
+                      GoogleFonts.acme(color: Colors.white, fontSize: 20)),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Umair Hashmi',
+                      style: GoogleFonts.almendra(
+                          color: Colors.white, fontSize: 40)),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text('Application Developer - (Android & IOS)',
+                      style: TextStyle(color: Colors.white, fontSize: 18)),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.orangeAccent),),
+                    onPressed: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 30,right: 30),
+                      child: Text('Hire Me',style: GoogleFonts.cutiveMono(color: Colors.black87,fontWeight: FontWeight.bold),),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.grey,),),
+                  iconButton(FontAwesomeIcons.facebook),
+                  iconButton(FontAwesomeIcons.twitter),
+                  iconButton(FontAwesomeIcons.instagram),
+                  iconButton(FontAwesomeIcons.linkedin),
+                  Expanded(child: Divider(color: Colors.grey,),),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: 20,
+            right: 20,
+            child: Container(
+              color: Colors.white,
+              child: IconButton(
+                icon: FaIcon(FontAwesomeIcons.gripLines,color: Colors.black,),
+                onPressed: (){
+                  _utils.showBottomSheet(context);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
